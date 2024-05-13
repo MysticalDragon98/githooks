61e63c879689229264668685c782b11f6ee06892
CamiloTDex <ctorres@dexfreight.io>
2024-05-13 18:44:03 -0500

    chore: Refactor loadConfig to use async/await syntax
    The code changes in `loadConfig.ts` refactor the `loadConfig` function to use the `async/await` syntax instead of the previous promise chaining. This improves the readability and maintainability of the code.

--- lib/modules/config/loadConfig.ts
+++ lib/modules/config/loadConfig.ts
@@ -2,12 +2,8 @@ import { $CONFIG_FILE } from "../../env";
 import { GithooksConfig } from "../../types/GithooksConfig.type";
 import readTomlFile from "../fs/readTomlFile";
 
-let ConfigCache: GithooksConfig = null;
-
 export default async function loadConfig (): Promise<GithooksConfig> {
-    if (ConfigCache) return ConfigCache;
-    
-    return ConfigCache = await readTomlFile<GithooksConfig>($CONFIG_FILE, {
+    return await readTomlFile<GithooksConfig>($CONFIG_FILE, {
         includes: []
     });
 }
