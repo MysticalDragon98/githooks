f906a8b302f4b9be76c1e053850584001cba36b4
CamiloTDex <ctorres@dexfreight.io>
2024-05-14 10:30:01 -0500

    ```text chore: Update notifyHTTPEndpoint to use normalized repository name
    The code changes in `notifyHTTPEndpoint.ts` update the logic to use the normalized repository name instead of the original repository name. This ensures that the correct project configuration is found when searching for the project config file.

--- lib/http/endpoints/webhooks/notify.http-endpoint.ts
+++ lib/http/endpoints/webhooks/notify.http-endpoint.ts
@@ -5,15 +5,16 @@ import executeBranchCommand from "../../../modules/sh/executeBranchCommand";
 import generateJobTempFile from "../../../modules/fs/generateJobTempFile";
 
 export default async function notifyHTTPEndpoint (push: IBitbucketPushWebhook, repository: IBitbucketPushWebhook["repository"], $request: any) {
-    const projectConfig = await findProjectConfig(repository.name);
+    const repositoryName = repository.full_name.split("/")[1];
+    const projectConfig = await findProjectConfig(repositoryName);
     const branchName = push.changes[0].new.name;
 
     if (!projectConfig) {
-        log("No project found for repository: " + repository.name);
+        log("No project found for repository: " + repositoryName);
         
         return {
             success: false,
-            message: "No project found for repository: " + repository.name
+            message: "No project found for repository: " + repositoryName
         }
     }
 
