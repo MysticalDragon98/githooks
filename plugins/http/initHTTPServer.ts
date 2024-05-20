b230264949c61fd727f0b4b7caf835ebb2fd3c6a
CamiloTDex <ctorres@dexfreight.io>
2024-05-20 13:41:20 -0500

    Add HTTP OK

--- plugins/http/initHTTPServer.ts
+++ plugins/http/initHTTPServer.ts
@@ -1,6 +1,8 @@
 import { Server } from "@coretools/http-server";
 import { $HTTP_PORT } from "../../lib/env";
 import webhooks from "../../lib/http/endpoints/webhooks/index";
+import { log } from "termx";
+import StyleOK from "../../lib/styles/OK.style";
 //* Imports
 
 const Endpoints = {
@@ -12,7 +14,9 @@ export async function initHTTPServer () {
     const server = new Server({
         endpoints: Endpoints,
         port: $HTTP_PORT,
-        
+        onStart () {
+            log(StyleOK(), "HTTP server started on port: " + $HTTP_PORT);
+        }
     });
 
     //* Plugins
